828690e09685ab3340bdce078cdd05aaa041c21d
Roy Keyes <psyclone241@gmail.com>
2016-06-09 23:56:53 -0400

    Changing pypi packages for version 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rollme',
-    version='0.0.1',
+    version='0.0.3',
     scripts=['rollme'],
     author='Roy Keyes',
     author_email='keyes.roy@gmail.com',
